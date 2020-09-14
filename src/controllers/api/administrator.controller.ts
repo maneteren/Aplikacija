cf3a3c95def14d573a73781be7c888f9643daa1b
Nikola Milanov <nikola.milanov93@gmail.com>
2020-09-14 19:50:30 +0200

    Dodatak

--- src/controllers/api/administrator.controller.ts
+++ src/controllers/api/administrator.controller.ts
@@ -4,28 +4,29 @@ import { AddAdministratorDto } from "../../dtos/administrator/add.administrator.
 import { EditAdministratorDto } from "../../dtos/administrator/edit.administrator.dto";
 import { AdministratorService } from "../../services/administrator/administrator.service";
 
+
 @Controller('api/administrator')
 export class AdministratorController {
     constructor(
         private administratorService: AdministratorService
     ) { }
 
-    
+
     // GET http://localhost:3000/api/administrator    
     @Get()
-    getAll(): Promise<Administrator[]>{
-     return this.administratorService.getAll();
+    getAll(): Promise<Administrator[]> {
+        return this.administratorService.getAll();
     }
 
     // GET http://localhost:3000/api/administrator/4/ "dostavi mi administratora sa id 4"
     @Get(':id')
-    getById(@Param('id') administratorId: number): Promise<Administrator>{
-     return this.administratorService.getById(administratorId);
+    getById(@Param('id') administratorId: number): Promise<Administrator> {
+        return this.administratorService.getById(administratorId);
     }
 
     // PUT http://localhost:3000/api/administrator
     @Put()
-    add(@Body() data: AddAdministratorDto): Promise<Administrator>{
+    add(@Body() data: AddAdministratorDto): Promise<Administrator> {
         return this.administratorService.add(data);
     }
 
